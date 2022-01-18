a5834d7040e4f38a1341ff8fb4700e9457aadbdf
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2022-01-18 12:55:47 +0000

    Align Started

--- ailab2/lab-steering2/code/steering3.cpp
+++ ailab2/lab-steering2/code/steering3.cpp
@@ -53,27 +53,23 @@ class Seek
 public:
 	//--I tried making these references but but got "E1790 - the default contructor of "Seek" cannot be referenced -- it is a deleted function"
 	//--when trying to create seek object in main method. 
-	Kinematic* character;
-	Kinematic* target;
-	float maxAcceleration;
+	Kinematic& character_;									//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	Kinematic& target_;
+	float maxAcceleration_;
 
 	Seek() = default;
 
-	Seek(Kinematic* character, Kinematic* target, float maxAcceleration) {
-		this->character = character;
-		this->target = target;
-		this->maxAcceleration = maxAcceleration;
-	}
+	Seek(Kinematic& character, Kinematic& target, float maxAcceleration) : character_{ character }, target_{ target }, maxAcceleration_{ maxAcceleration }	{ }
 
 	SteeringOutput GetSteering() {
 		SteeringOutput result;
 
 		//--Get the direction to the target
-		result.linear = target->position - character->position;
+		result.linear = target_.position - character_.position;
 
 		//--Give the full acceleration along this direction
 		result.linear.normalise();
-		result.linear *= maxAcceleration;
+		result.linear *= maxAcceleration_;
 
 		result.angular = 0;
 		return result;
@@ -84,7 +80,7 @@ class Ship : public Kinematic {
 public:
 	ai::Vector2 l, r, nose;
 	ai::Vector2 pos;
-	float rotation;
+	//float rotation;
 	raylib::Color col = RED;
 
 	/*Ship() {
@@ -103,7 +99,7 @@ public:
 	void Draw() {
 		pos.x = position.x;
 		pos.y = position.z;
-		ai::DrawTrianglePro(pos, l, r, nose, rotation, col);
+		ai::DrawTrianglePro(pos, l, r, nose, rotation, col);		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!should rotation be orientation
 
 	}
 };
@@ -131,7 +127,7 @@ public:
 	}
 
 	SteeringOutput GetSteering() {
-		SteeringOutput result;
+		SteeringOutput result{};
 
 		//--Get the direction to the target
 		Vector direction = target->position - character->position;
@@ -139,15 +135,15 @@ public:
 
 		//--Check if we are there, return no steering
 		if (distance <= targetRadius) {
-			result.linear = { 0,0,0 };
-			//result.linear = { -character->velocity.x, 0, -character->velocity.z };
-			result.angular = 0;
+			//result.linear = { 0,0,0 };
+			//result.linear = { -character->velocity.x, 0, -character->velocity.z };		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+			//result.angular = 0;
 			return result;
 			//return __null;				// this is probably wrong
 		}
 
 		//--If we are outside the slow radius then move at max speed
-		float targetSpeed = 0;			// using name from pseudocode p.99 ln.30, doesn't really make sende to me (characterSpeed better name?)
+		float targetSpeed{};			// using name from pseudocode p.99 ln.30, doesn't really make sende to me (characterSpeed better name?)
 		if (distance > slowRadius)
 			targetSpeed = maxSpeed;
 		//--Otherwise calculate scaled speed
@@ -174,6 +170,39 @@ public:
 	}
 };
 
+class Align {
+public:
+	Kinematic& character_;
+	Kinematic& target_;
+
+	float maxAngularAcceleration_ = 1.0f;
+	float maxRotation_ = 3.0f;
+
+	float targetRadius;				// the radius for arriving at the target
+	float slowRadius;				// the radius for beginning to slow down
+	float timeToTarget = 0.1f;		// the time over which to achieve target speed
+
+	SteeringOutput GetSteering() {
+		SteeringOutput result;
+
+		//--Get the direction to the target
+		float rotation = target_.orientation - character_.orientation;
+
+		//Map the resut to the (-pi,pi) interval
+		rotation = MapToRange(rotation);
+		float rotationSize = abs(rotation);
+
+		return result;
+	}
+
+	float MapToRange(float rotation) {						//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		float modRotation   = fmod(rotation, 360);
+		float rotationRad = rotation * DEG2RAD;
+	}
+
+	
+};
+
 int main(int argc, char* argv[])
 {
 	float sw{ 1024 }, sh{ 768 };
@@ -183,16 +212,17 @@ int main(int argc, char* argv[])
 
 	//--Declare Ships
 	Ship* ship = new Ship({ sw / 2,0,sh / 2 }, { 0,-10 }, { 0,10 }, { 30,0 }, 0, BLUE);
+	//Ship ship2({ sw / 2,0,sh / 2 }, { 0,-10 }, { 0,10 }, { 30,0 }, 0, BLUE);
 	Ship* enemy = new Ship({ sw / 2,0,sh / 2 - 200 }, { 0,-10 }, { 0,10 }, { 30,0 }, 0, RED);
 
 
 	//--Declare Seek
-	Seek* seekEnemy = new Seek(ship, enemy, 1.0f);
-	Seek* seekShip = new Seek(enemy, ship, 1.0f);
+	Seek* seekEnemy = new Seek(*ship, *enemy, 1.0f);
+	Seek* seekShip = new Seek(*enemy, *ship, 1.0f);
 
 	//--Declare Arrive
-	Arrive arriveEnemy = *(new Arrive(ship, enemy, 3, 10, 5, 200));
-	Arrive arriveShip = *(new Arrive(enemy, ship, 1, 3, 3, 100));
+	Arrive arriveEnemy = *(new Arrive(ship, enemy, 140, 200, 10, 200));
+	Arrive arriveShip = *(new Arrive(enemy, ship, 70, 100, 10, 100));
 
 	while (!window.ShouldClose()) // Detect window close button or ESC key
 	{
@@ -211,15 +241,12 @@ int main(int argc, char* argv[])
 		ship->Draw();
 		//ai::DrawTrianglePro({ sw / 2,sh / 2 }, { 0,-10 }, { 0,10 }, { 30,0 }, 0, BLUE);
 
-
 		EndDrawing();
 
-		//ship->Update(seekEnemy->GetSteering(), 3.0f, 1.0f);
-		//enemy->Update(seekShip->GetSteering(), 3.0f, 1.0f);
+		ship->Update(arriveEnemy.GetSteering(), 200.0f, GetFrameTime());
+		//enemy->Update(arriveShip.GetSteering(), 100.0f, GetFrameTime());
 
-		ship->Update(arriveEnemy.GetSteering(), 5.0f, 0.5f);
-		//enemy->Update(arriveShip.GetSteering(), 3.0f, 1.0f);
-
+		
 	}
 
 	return 0;
